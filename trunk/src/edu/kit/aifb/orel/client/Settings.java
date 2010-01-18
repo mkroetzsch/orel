71cdda8b07b02d9da39d969dfef2c48b29487a0b
mak@aifb.uni-karlsruhe.de <mak@aifb.uni-karlsruhe.de@542ed5ba-012e-11df-b136-7f7962f7bc17>
2010-01-18 15:44:50 +0000

    remove unused import

--- trunk/src/edu/kit/aifb/orel/client/Settings.java
+++ trunk/src/edu/kit/aifb/orel/client/Settings.java
@@ -3,7 +3,6 @@ package edu.kit.aifb.orel.client;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Properties;
