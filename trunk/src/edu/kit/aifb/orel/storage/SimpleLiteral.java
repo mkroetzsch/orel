2ecf24812af53615f5e1dc8e4d2e1d2886a00c6c
mak@aifb.uni-karlsruhe.de <mak@aifb.uni-karlsruhe.de@542ed5ba-012e-11df-b136-7f7962f7bc17>
2010-02-11 09:46:51 +0000

    basic loading/nomralisation support for literal values

--- trunk/src/edu/kit/aifb/orel/storage/SimpleLiteral.java
+++ trunk/src/edu/kit/aifb/orel/storage/SimpleLiteral.java
@@ -3,18 +3,21 @@ package edu.kit.aifb.orel.storage;
 /**
  * The simplest possible literal container. It is assumed that this literal
  * is always used with a normalized (canonical) lexical value. Typically, the
- * given datatype is the most specific one that the literal belongs to, i.e.
- * the intersection of all datatypes it belongs to. Since not all intersections
- * of relevant XML Schema types have XSD URIs, some datatype URIs used here are 
- * specific to Orel.
+ * given datatype is the most general one that the literal belongs to, e.g. it.
+ * is owl_real for any numerical type. The valueString is not the lexical value
+ * but rather a canonical string version of the semantic value. For example, it 
+ * the same string "1" would be used for "1"^^xsd:int and "2/2"^^owl:rational,
+ * even though "1" is not in the lexical space of owl:rational.
  * @author Markus Krötzsch
  */
 public class SimpleLiteral {
-	String lexicalValue;
+	String valueString;
 	String datatypeURI;
+	Object value;
 	
-	public SimpleLiteral(String canonicalValue, String datatypeURI) {
-		this.lexicalValue = canonicalValue;
+	public SimpleLiteral(String valueString, Object value, String datatypeURI) {
+		this.valueString = valueString;
+		this.value = value;
 		this.datatypeURI = datatypeURI;
 	}
 	
@@ -22,16 +25,22 @@ public class SimpleLiteral {
 		return datatypeURI;
 	}
 	
-	public String getLexicalValue() {
-		return lexicalValue;
+	public String getValueString() {
+		return valueString;
+	}
+	
+	public Object getvalue() {
+		return value;
 	}
 	
 	/**
 	 * Create a string representation for this value that resembles the representation
-	 * used in Turtle. However, we do not care about escaping issues here.
+	 * used in Turtle. However, we do not care about escaping issues here. Since we
+	 * use the value string to uniquely identify the (semantic) value, it may not even
+	 * be a valid lexical value for the datatype.
 	 */
 	public String toString() {
-		return '"' + lexicalValue + '"' + "^^" + datatypeURI; 
+		return '"' + valueString + '"' + "^^" + datatypeURI; 
 	}
 
 }
